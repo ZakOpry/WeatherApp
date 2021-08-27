306cfac5974f32735c209293de3fab61d7df98d5
ZakOpry <88099668+ZakOpry@users.noreply.github.com>
2021-08-27 15:25:58 -0400

    weatherApp

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+const weatherDisplay = document.querySelector('.weather')
+const button = document.querySelector('.button')
+
+
+async function pressButton(){
+const input = document.querySelector('.input').value 
+const cityData = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&units=imperial&appid=ff1cadec65d9f10c3aafc2a01b14612c`)
+const cityDataJson = await cityData.json()
+
+weatherDisplay.innerHTML = ""   
+const createTemp = document.createElement('p')
+const createDisc = document.createElement('P')
+createTemp.innerHTML = cityDataJson.main.temp + ' °F'
+createDisc.innerHTML = cityDataJson.weather[0].description
+console.log(createTemp)
+weatherDisplay.append(createTemp, createDisc)
+
+
+}
+
+
+
+
+
+
+
+button.addEventListener("click", pressButton)
